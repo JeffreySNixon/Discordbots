c581a22c964c95c6791dbd87a27f168114f0dee6
Jeffrey S Nixon <55815379+JeffreySNixon@users.noreply.github.com>
2020-02-27 00:16:54 -0600

    Updating number of games

--- commands/Role.js
+++ commands/Role.js
@@ -27,6 +27,7 @@ class Role extends Command {
                 .then(message.delete())
                 .then(console.log('Added MonsterHunter role'))
                 .then(message.send('Role Added'))
+                .then(delay(1000))
                 .then(message.delete())
                 .catch(console.error);
         
