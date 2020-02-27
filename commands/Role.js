f59b3c8a5bb33dabdaa8843947f237e13efefdea
Jeffrey S Nixon <55815379+JeffreySNixon@users.noreply.github.com>
2020-02-27 00:47:01 -0600

    Updating number of games

--- commands/Role.js
+++ commands/Role.js
@@ -26,103 +26,103 @@ class Role extends Command {
                 message.member.addRole('669963991434526745')
                 .then(console.log('Added MonsterHunter role'))
                 .then(message.delete(1000))
-                .then(message.channel.send('Role Added').then(message.delete(1000)))
+                .then(message.channel.send('Role Added').then(d_msg => {d_msg.delete(5000)}))
                 .catch(console.error);
         
                 break;
             case 'LOL':
                 message.member.addRole('681725389835337845')
                 .then(console.log('Added LOL role'))
-                .then(message.reply('Role Added'))
+                .then(message.reply('Role Added').then(d_msg => {d_msg.delete(5000)}))
                 .catch(console.error);
             
                 break;       
             case 'PUBG':
                 message.member.addRole('681725702281625610')
                 .then(console.log('Added PUBG role'))
-                .then(message.reply('Role Added'))
+                .then(message.reply('Role Added').then(d_msg => {d_msg.delete(5000)}))
                 .catch(console.error);
                 
             break;
             case 'Division2':
                 message.member.addRole('681725469200220214')
                 .then(console.log('Added Division2 role'))
-                .then(message.reply('Role Added'))
+                .then(message.reply('Role Added').then(d_msg => {d_msg.delete(5000)}))
                 .catch(console.error);
         
                 break;
             case 'Satisfactory':
                 message.member.addRole('681725525781250118')
                 .then(console.log('Added Satisfactory role'))
-                .then(message.reply('Role Added'))
+                .then(message.reply('Role Added').then(d_msg => {d_msg.delete(5000)}))
                 .catch(console.error);
             
                 break;
             case 'Minecraft':
                 message.member.addRole('681725569322450946')
                 .then(console.log('Added Minecraft role'))
-                .then(message.reply('Role Added'))
+                .then(message.reply('Role Added').then(d_msg => {d_msg.delete(5000)}))
                 .catch(console.error);
             
                 break;
             case 'CallOfDuty':
                 message.member.addRole('681725596002549800')
                 .then(console.log('Added CallOfDuty role'))
-                .then(message.reply('Role Added'))
+                .then(message.reply('Role Added').then(d_msg => {d_msg.delete(5000)}))
                 .catch(console.error);
             
                 break;
             case 'SoloGames':
                 message.member.addRole('681725634887548957')
                 .then(console.log('Added SoloGames role'))
-                .then(message.reply('Role Added'))
+                .then(message.reply('Role Added').then(d_msg => {d_msg.delete(5000)}))
                 .catch(console.error);
             
                 break;
             case 'Terraria':
                 message.member.addRole('681725681444716567')
                 .then(console.log('Added Terraria role'))
-                .then(message.reply('Role Added'))
+                .then(message.reply('Role Added').then(d_msg => {d_msg.delete(5000)}))
                 .catch(console.error);
             
                 break;
             case 'Apex':
                 message.member.addRole('681725504092372992')
                 .then(console.log('Added Terraria role'))
-                .then(message.reply('Role Added'))
+                .then(message.reply('Role Added').then(d_msg => {d_msg.delete(5000)}))
                 .catch(console.error);
             
                 break;
             case 'WoW':
                 message.member.addRole('682466541916520460')
                 .then(console.log('Added MonsterHunter role'))
-                .then(message.reply('Role Added'))
+                .then(message.reply('Role Added').then(d_msg => {d_msg.delete(5000)}))
                 .catch(console.error);
         
                 break;
             case 'WoWClassic':
                 message.member.addRole('682466564192469022')
                 .then(console.log('Added MonsterHunter role'))
-                .then(message.reply('Role Added'))
+                .then(message.reply('Role Added').then(d_msg => {d_msg.delete(5000)}))
                 .catch(console.error);
         
                 break;
             case 'Destiny2':
                 message.member.addRole('682466583875944448')
                 .then(console.log('Added MonsterHunter role'))
-                .then(message.reply('Role Added'))
+                .then(message.reply('Role Added').then(d_msg => {d_msg.delete(5000)}))
                 .catch(console.error);
         
                 break;
             case 'Civ6':
                 message.member.addRole('682466519195844638')
                 .then(console.log('Added MonsterHunter role'))
-                .then(message.reply('Role Added'))
+                .then(message.reply('Role Added').then(d_msg => {d_msg.delete(5000)}))
                 .catch(console.error);
         
                 break;    
             case 'Error':
-                message.reply('Error has occured')
+                message.reply('Error has occured').then(d_msg => {d_msg.delete(5000)})
                 .catch(console.error);
                 break;
         }
--- commands/RoleR.js
+++ commands/RoleR.js
@@ -25,102 +25,102 @@ class RoleR extends Command {
             case 'MonsterHunter':
                 message.member.removeRole('669963991434526745')
                 .then(console.log('Removed MonsterHunter role'))
-                .then(message.reply('Role Removed'))
+                .then(message.reply('Role Removed').then(d_msg => {d_msg.delete(5000)}))
                 .catch(console.error);
         
                 break;
             case 'LOL':
                 message.member.removeRole('681725389835337845')
                 .then(console.log('Removed LOL role'))
-                .then(message.reply('Role Removed'))
+                .then(message.reply('Role Removed').then(d_msg => {d_msg.delete(5000)}))
                 .catch(console.error);
             
                 break;       
             case 'PUBG':
                 message.member.removeRole('681725702281625610')
                 .then(console.log('Removed PUBG role'))
-                .then(message.reply('Role Removed'))
+                .then(message.reply('Role Removed').then(d_msg => {d_msg.delete(5000)}))
                 .catch(console.error);
                 
             break;
             case 'Division2':
                 message.member.removeRole('681725469200220214')
                 .then(console.log('Removed Division2 role'))
-                .then(message.reply('Role Removed'))
+                .then(message.reply('Role Removed').then(d_msg => {d_msg.delete(5000)}))
                 .catch(console.error);
         
                 break;
             case 'Satisfactory':
                 message.member.removeRole('681725525781250118')
                 .then(console.log('Removed Satisfactory role'))
-                .then(message.reply('Role Removed'))
+                .then(message.reply('Role Removed').then(d_msg => {d_msg.delete(5000)}))
                 .catch(console.error);
             
                 break;
             case 'Minecraft':
                 message.member.removeRole('681725569322450946')
                 .then(console.log('Removed Minecraft role'))
-                .then(message.reply('Role Removed'))
+                .then(message.reply('Role Removed').then(d_msg => {d_msg.delete(5000)}))
                 .catch(console.error);
             
                 break;
             case 'CallOfDuty':
                 message.member.removeRole('681725596002549800')
                 .then(console.log('Removed CallOfDuty role'))
-                .then(message.reply('Role Removed'))
+                .then(message.reply('Role Removed').then(d_msg => {d_msg.delete(5000)}))
                 .catch(console.error);
             
                 break;
             case 'SoloGames':
                 message.member.removeRole('681725634887548957')
                 .then(console.log('Removed SoloGames role'))
-                .then(message.reply('Role Removed'))
+                .then(message.reply('Role Removed').then(d_msg => {d_msg.delete(5000)}))
                 .catch(console.error);
             
                 break;
             case 'Terraria':
                 message.member.removeRole('681725681444716567')
                 .then(console.log('Removed Terraria role'))
-                .then(message.reply('Role Removed'))
+                .then(message.reply('Role Removed').then(d_msg => {d_msg.delete(5000)}))
                 .catch(console.error);
             
                 break;
             case 'Apex':
                 message.member.removeRole('681725504092372992')
                 .then(console.log('Removed Terraria role'))
-                .then(message.reply('Role Removed'))
+                .then(message.reply('Role Removed').then(d_msg => {d_msg.delete(5000)}))
                 .catch(console.error);
             
                 break;
             case 'WoW':
                 message.member.removeRole('682466541916520460')
                 .then(console.log('Removed MonsterHunter role'))
-                .then(message.reply('Role Removed'))
+                .then(message.reply('Role Removed').then(d_msg => {d_msg.delete(5000)}))
                 .catch(console.error);
         
                 break;
             case 'WoWClassic':
                 message.member.removeRole('682466564192469022')
                 .then(console.log('Removed MonsterHunter role'))
-                .then(message.reply('Role Removed'))
+                .then(message.reply('Role Removed').then(d_msg => {d_msg.delete(5000)}))
                 .catch(console.error);
         
                 break;
             case 'Destiny2':
                 message.member.removeRole('682466583875944448')
                 .then(console.log('Removed MonsterHunter role'))
-                .then(message.reply('Role Removed'))
+                .then(message.reply('Role Removed').then(d_msg => {d_msg.delete(5000)}))
                 .catch(console.error);
         
                 break;
             case 'Civ6':
                 message.member.removeRole('682466519195844638')
                 .then(console.log('Removed MonsterHunter role'))
-                .then(message.reply('Role Removed'))
+                .then(message.reply('Role Removed').then(d_msg => {d_msg.delete(5000)}))
                 .catch(console.error);
             
             case 'Error':
-                message.reply('Error has occured')
+                message.reply('Error has occured').then(d_msg => {d_msg.delete(5000)})
                 .catch(console.error);
                 break;
         }
